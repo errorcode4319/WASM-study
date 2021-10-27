fca56644ad86caf091d5508a470d2d43f8486676
errorcode4319 <sandlekang@gmail.com>
2021-10-28 00:21:05 +0900

    chap3 done

--- ch3/3.6_side_module/side_module.c
+++ ch3/3.6_side_module/side_module.c
@@ -1,6 +1,7 @@
 #include <emscripten.h>
 
 //https://githubmemory.com/repo/gwsounddsg/WasmTesting/issues/2
+//zsh: no matches found: EXPORTED_RUNTIME=[]
 
 int Increment(int value) {
     return (value + 1);
